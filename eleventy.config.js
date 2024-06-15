aede0198e4fd33f1b69c4b8f1708eeac3e9efce3
Nathan Bottomley <nathan.bottomley@gmail.com>
2024-06-15 16:59:32 +1000

    Initial commit

---
+++ eleventy.config.js
@@ -0,0 +1,5 @@
+import podcastPlugin from './podcast.config.js'
+
+export default function(eleventyConfig) {
+  eleventyConfig.addPlugin(podcastPlugin)
+}
---
+++ podcast.config.js
@@ -0,0 +1,5 @@
+import rssPlugin from '@11ty/eleventy-plugin-rss'
+
+export default function(eleventyConfig) {
+  eleventyConfig.addPlugin(rssPlugin)
+}
